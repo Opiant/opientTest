67f0c87f23bedcb2ea341c95cd6a79dcd768e4d1
Hiren C. Mistry <hirenhcm@gmail.com>
2014-09-13 22:44:41 +0400

    mas

--- server/app.js
+++ server/app.js
@@ -4,3 +4,22 @@ function mainMaster(){
 function mainFeature(){
 	console.log(123);
 }
+
+function mai123nMaster(){
+	console.log(123);
+}
+
+
+function ma123inFeature(){
+	console.log(123);
+}
+
+
+function ma123inMaster(){
+	console.log(123);
+}
+
+
+function mainF123eature(){
+	console.log(123);
+}
---
+++ server/controller.js
@@ -0,0 +1,7 @@
+
+
+
+
+
+asf asf asf sdf sdf sdf sd sdf 
+dfg sd gdg dfg dfg dfg dg df 
